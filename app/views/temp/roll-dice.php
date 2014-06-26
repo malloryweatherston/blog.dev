d0cb2a25928dfaca2954008f967c0a2fe6c30bfb
malloryweatherston <mjw0909@sbcglobal.net>
2014-06-26 13:49:56 -0500

    Added message to alert if the guessed number is the same as the random number

--- app/views/temp/roll-dice.php
+++ app/views/temp/roll-dice.php
@@ -14,5 +14,8 @@ $rand =  rand ($min, $max);
 <body>
 	<h1>Guess: <?= $guess; ?></h1>
     <h1>Random Number: <?= $rand; ?></h1>
+    <? if ($rand == $guess) : ?>
+    <h1><? echo "Your guess matches the random number!"; ?><h1>
+    <? endif; ?>
 </body>
 </html>
