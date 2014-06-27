f33cd7a1812d5762a43fa5a4a2aee63968e17e2d
malloryweatherston <mjw0909@sbcglobal.net>
2014-06-27 09:36:44 -0500

    Add content to Resume and Portfolio and extend master.blade.php

---
+++ app/views/pages/portfolio.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.master')
+
+
+
+@section('contentportfolio')
+    <h1>My Portfolio</h1>
+@stop
---
+++ app/views/pages/resume.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('contentresume')
+    <h1>My Resume</h1>
+@stop
