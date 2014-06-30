eb9104d734be3b43eed0310d17e67082827cd065
malloryweatherston <mjw0909@sbcglobal.net>
2014-06-30 16:42:33 -0500

    "Added new formatting to the file."

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -1,10 +1,27 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Laravel Blog</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Mallory Weatherston</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="spot/theme/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="spot/theme/assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="spot/theme/assets/css/main.css" rel="stylesheet">
+
 </head>
 <body>
-    @yield('contentresume')
-    @yield('postscontent')
+    @yield('content')
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
