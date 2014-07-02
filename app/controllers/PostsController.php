a9e65c6edd1bf3503b43c27c39ee7b0e31324bb3
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-02 14:22:34 -0500

    Added construct to filter for basic auth.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -1,7 +1,16 @@
 <?php
 
 class PostsController extends \BaseController {
+	
+	public function __construct()
+	{
+	    // call base controller constructor
+	    parent::__construct();
 
+	    // run auth filter before all methods on this controller except index and show
+	    $this->beforeFilter('auth.basic', array('except' => array('index', 'show')));
+	}
+	
 	/**
 	 * Display a listing of the resource.
 	 *
