8dfe7e3c8623f29148b70c076413415c2132d716
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-03 15:14:56 -0500

    Modified several routes.

--- app/routes.php
+++ app/routes.php
@@ -37,43 +37,5 @@ Route::get('/sayhello/{name}', 'HomeController@sayHello');
 
 Route::resource('posts', 'PostsController');
 
-// Route::get('/index', function () {
- //    $post = new Post();
- //    $post1 = new Post();
-	// $post1->title = Input::get('Title');
-	// $post1->body = Input::get('Body');
-	// $post1->save();
-	// $posts = Post::all();
 
-	// foreach ($posts as $post) {
-	// 	echo $post->title . "<br>";
-	// 	echo $post->body . "<br>";
-	// }
-	// $post = Post::find(1);
-	// echo $post->title . "<br>";
-	// echo $post->body . "<br>";
- //    $post->title = "This is a NEW title";
-
-
-    //return 'Eloquent ORM';
-
-// Route::get('/show{id}', function($id){
-// 	$post = Post::find($id);
-// 	echo $post->title . "<br>";
-// 	echo $post->body . "<br>";
- 	//$post->title = "This is a NEW title";
-
-// });
-// Route::get('/create', function() {
-// 	return View::make('posts.create');
-// });
-
-// Route::get('/rolldice/{guess}', function($guess){
-// 	$rand =  rand (1, 6);
-// 	$data = array(
-// 		'rand' => $rand,
-// 		'guess' => $guess
-// 	);
-// 	return View::make('temp.roll-dice')->with($data);
-// });
 
