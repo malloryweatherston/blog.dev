f8094930d3d2d513cb210ffc973cbe73751ac3ab
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-03 11:56:52 -0500

    Modified index in PostController.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -18,11 +18,14 @@ class PostsController extends \BaseController {
 	 */
 	public function index()
 	{
-		$posts = Post::paginate(4);
-
+		if (Input::has('search')){
+			$search = Input::get('search');
+			$posts = Post::where("title", "LIKE", "%$search%")->paginate(4);
 			return View::make('posts.index')->with('posts', $posts);
-
-
+		} else {
+			$posts = Post::paginate(4);
+		}
+		return View::make('posts.index')->with('posts', $posts);
 	}
 
 
