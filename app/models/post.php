932cf991134bb33c26742df9ccb5937a724b8b33
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-07 16:01:50 -0500

    Created function addUploadedImage.

--- app/models/post.php
+++ app/models/post.php
@@ -5,15 +5,24 @@ class Post extends BaseModel {
 	//The db table this model relates to 
     protected $table = 'posts';
 
+    protected $imgDir = 'img-upload';
+
 
     //Validation rules for our model properties 
     static public $rules = [
     	'title' => 'required|max:100',
-    	'body' => 'required|max:2000'
+    	'body' => 'required|max:5000'
     ];
 
     public function user()
 	{
     	return $this->belongsTo('User');
 	}
+
+    public function addUploadedImage($image) {
+        $systemPath = public_path() . '/' . $this->imgDir . '/';
+        $imageName = $this->id . '-' . $image->getClientOriginalName();
+        $image->move($systemPath, $imageName);
+        $this->img_path =  '/' . $this->imgDir . '/' . $imageName;
+    }
 }
