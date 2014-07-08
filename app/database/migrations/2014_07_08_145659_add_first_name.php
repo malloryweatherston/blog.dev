e3e1685c925e2a507c57ae6ddadc1b3842b376b5
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-08 10:06:20 -0500

    Created Database seeders.

---
+++ app/database/migrations/2014_07_08_145659_add_first_name.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFirstName extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->string('first_name', 200)->nullable();
+			$table->string('last_name', 200)->nullable();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('first_name');
+			$table->dropColumn('last_name');
+		});
+	}
+
+}
