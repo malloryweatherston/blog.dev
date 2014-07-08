65bc95dcd1cd5c351c50ed3d9722648f7555c702
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-08 11:57:46 -0500

    Created a view for the login with email and password.

---
+++ app/views/pages/login.blade.php
@@ -0,0 +1,28 @@
+@extends('layout.master')
+
+@section('content')
+    <!DOCTYPE html>
+    <div align= "center">
+	     <div id="blue">
+		<div class="container">
+			<div class="row centered">
+				<div class="col-lg-8 col-lg-offset-2">
+				<h2>PLEASE LOG IN</h2>
+				</div>
+			</div><!-- row -->
+		</div><!-- container -->
+	</div><!--  bluewrap -->
+	    <br>
+	    {{ Form::open(array('action' => 'HomeController@doLogin')) }}
+	    {{ Form::label('email', 'Email:')}}
+		{{ Form::text('email')}}
+		<br>
+		{{ Form::label('password', 'Password:')}}
+		{{ Form::password('password')}}
+		<br>
+		<button type="Submit">Log In</button>
+
+
+	    {{ Form::close() }}
+	</div>
+@stop
