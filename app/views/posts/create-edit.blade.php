fea3ab253d158d5117d6e40a9e3b95439196b72c
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-09 13:53:42 -0500

    Adding button bar for text editing.

--- app/views/posts/create-edit.blade.php
+++ app/views/posts/create-edit.blade.php
@@ -1,5 +1,8 @@
 @extends('layout.master')
+ 
+@section('topscript')
 
+<link rel="stylesheet" type="text/css" href="../demo.css" />
 @section('content')
 
  <div id="blue">
@@ -18,29 +21,46 @@
 	</div><!--  bluewrap -->
 <br>
 <div align="center">
-@if (isset($post))
-	{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'files' => true)) }}
-@else
-	{{ Form::open(array('action' => 'PostsController@store', 'files' => true)) }}
-@endif
+				@if (isset($post))
+					{{ Form::model($post, array('action' => array('PostsController@update', $post->id), 'method' => 'PUT', 'files' => true)) }}
+				@else
+					{{ Form::open(array('action' => 'PostsController@store', 'files' => true)) }}
+				@endif
 
-@if ($errors->has('title'))
-	{{$errors->first('title', '<span class="help-block">:message</span>')}}
-@endif
-@if ($errors->has('body'))
-	{{$errors->first('body', '<span class="help-block">:message</span>')}}
-@endif
-	{{ Form::label('title', 'Title:')}}
-	{{ Form::text('title')}}
-	<br>
-	{{ Form::label('image', 'Image Upload:')}}
-	{{ Form::file('image')}}
-	<br>
-	{{ Form::label('body', 'Body:')}} {{ Form::textarea('body')}}
-	<br>
-	<button type="Submit">Create</button>
-
-
-{{ Form::close() }}
+				@if ($errors->has('title'))
+					{{$errors->first('title', '<span class="help-block">:message</span>')}}
+				@endif
+				@if ($errors->has('body'))
+					{{$errors->first('body', '<span class="help-block">:message</span>')}}
+				@endif
+					{{ Form::label('title', 'Title:')}}
+					{{ Form::text('title')}}
+					<br>
+					{{ Form::label('image', 'Image Upload:')}}
+					{{ Form::file('image')}}
+					<br>
+					<label for "wmd-input">Body:</label>
+						<div class="wmd-panel">
+            				<div id="wmd-button-bar"></div>
+					 		{{ Form::textarea('body', null, array('class'=>'wmd-input', 'id'=>'wmd-input')) }}
+							<br>
+							<button type="Submit">Create</button>
+							{{ Form::close() }}
+						</div>
+					</div>
+				<div id="wmd-preview" class="wmd-panel wmd-preview"></div>
 </div>
+
+@section('bottomscript')
+<script type="text/javascript" src="/../../Markdown.Converter.js"></script>
+<script type="text/javascript" src="/../../Markdown.Sanitizer.js"></script>
+<script type="text/javascript" src="/../../Markdown.Editor.js"></script>
+<script type="text/javascript">
+	$(function () {
+     	var converter = new Markdown.Converter();
+     	var editor = new Markdown.Editor(converter);
+     	editor.run();
+     });
+</script>
+
 @stop
