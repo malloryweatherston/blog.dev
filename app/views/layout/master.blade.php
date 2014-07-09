b7b7cbffc1c07406931fd781910e5e140210d016
malloryweatherston <mjw0909@sbcglobal.net>
2014-07-09 13:52:20 -0500

    Modifed view content.

--- app/views/layout/master.blade.php
+++ app/views/layout/master.blade.php
@@ -13,6 +13,8 @@
     <!-- Bootstrap core CSS -->
     <link href="/spot/theme/assets/css/bootstrap.css" rel="stylesheet">
     <link href="/spot/theme/assets/css/font-awesome.min.css" rel="stylesheet">
+   
+
 
     <!-- Custom styles for this template -->
     <link href="/spot/theme/assets/css/main.css" rel="stylesheet">
@@ -63,10 +65,12 @@
 		@endif
 	</div>
 
-    
+    @yield('topscript')
     @yield('content')
+    @yield('bottomscript')
 
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <script src="/assets/js/bootstrap.min.js"></script>
+    
 </body>
 </html>
